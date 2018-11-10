034cdc09d2f8e5c6a225929922c06a7ac678d47d
CHUNG-WEI <a2249564@gmail.com>
2018-11-11 04:51:07 +0800

    1.修正完地圖 2.新增物件座標陣列（有=1,沒有=0） 3.缺動畫部份（之後會補上） 4.blocky連結串接部分

--- GCGJ/function.js
+++ GCGJ/function.js
@@ -5,37 +5,60 @@ $(document).ready(function(){
         // $(".bg").append(
         //     '<div class="grid">'+(i+1)+'</div>'
         // );
+
+        // let playerX = 0;
+        // let playerY = 0;
+      
+        // $(".btn").on("click", function(){
+        //     playerX+=100;
+        //     $(".player").css("left", playerX+ "px");              
+        // });
+
+        var rockArray = new Array(100);
+        var monsterArray = new Array(100);
+        var treeArray = new Array(100);
+
+        rockArray[i] = 0;
+        monsterArray[i] = 0;
+        treeArray[i] = 0;
+
         var randomG=Math.floor(Math.random()*(6-0));
+        var locationX = i%10 * 100;
+        var locationY = Math.floor(i/10) * 100;
         if(randomG==0){
-            $(".bg").append('<img class="my_image" src="./image/ground/gcgj_grass.png"/>');
+            $(".bg").append('<img class="grass" id = grass' + i + ' src="./image/ground/gcgj_grass.png"/>');
         }
         else if(randomG==1){
-            $(".bg").append('<img class="my_image" src="./image/ground/gcgj_s_g.png"/>');
+            $(".bg").append('<img class="grass" id = grass' + i + ' src="./image/ground/gcgj_s_g.png"/>');
         }  
         else if(randomG==2){
-            $(".bg").append('<img class="my_image" src="./image/ground/gcgj_s_g2.png"/>');
+            $(".bg").append('<img class="grass" id = grass' + i + ' src="./image/ground/gcgj_s_g2.png"/>');
         }  
         else if(randomG==3){
-            $(".bg").append('<img class="my_image" src="./image/ground/gcgj_s_g3.png"/>');
+            $(".bg").append('<img class="grass" id = grass' + i + ' src="./image/ground/gcgj_s_g3.png"/>');
         }  
         else if(randomG==4){
-            $(".bg").append('<img class="my_image" src="./image/ground/gcgj_s_g4.png"/>');
+            $(".bg").append('<img class="grass" id = grass' + i + ' src="./image/ground/gcgj_s_g4.png"/>');
         }  
         else if(randomG==5){
-            $(".bg").append('<img class="my_image" src="./image/ground/gcgj_soil.png"/>');
+            $(".bg").append('<img class="grass" id = grass' + i + ' src="./image/ground/gcgj_soil.png"/>');
         }
+        
+        $("#grass"+i).css("left", locationX + "px");
+        $("#grass"+i).css("top", locationY + "px");
 
         var randomR=Math.floor(Math.random()*(20-0));
         if(randomR==0){
-            $(".bg").append('<img class="my_image" src="./image/ground/gcgj_rock.png"/>');
+            $(".bg").append('<img class="rock" id = rock' + i + ' src="./image/ground/gcgj_rock.png"/>');
+            $("#rock"+i).css("left", locationX + "px");
+            $("#rock"+i).css("top", locationY + "px");
+            rockArray[i] = 1;
+            //rock(i);
         }
-      }
-      let playerX = 0;
-      let playerY = 0;
+    }
+    // function rock(i){   
+    //     console.log(i);
+    // }
       
-      $(".btn").on("click", function(){
-        playerX+=100;
-        $(".player").css("left", playerX+ "px");
-                
-      });
-});
+}
+);
