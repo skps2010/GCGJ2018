5d40dda44571b2fe1ed9ce0bdb8784ad3d39c70f
CHUNG-WEI <a2249564@gmail.com>
2018-11-10 21:34:39 +0800

    no message

--- GCGJ/game/main.js
+++ GCGJ/game/main.js
@@ -1,8 +1,28 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
+
 ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
+// ctx.rect(20, 40, 50, 50);
+// ctx.fillStyle = "#FF0000";
+// ctx.fill();
+// ctx.closePath();
+// ctx.fillRect(25,25,100,100);
+// ctx.clearRect(45,45,60,60);
+var img = new Image();
+img.onload = function(){
+    context.drawImage(img, 0, 0);
+};
+img.src = "http://www.w3.org/html/logo/img/one-color.png";
+ctx.strokeRect(0,0,1000,800);
+for(i=0;i<1000;i=i+100){
+    ctx.moveTo(i,0);
+    ctx.lineTo(i,800);
+    ctx.stroke();
+}
+for(i=0;i<800;i=i+80){
+    ctx.moveTo(0,i);
+    ctx.lineTo(1000,i);
+    ctx.stroke();
+}
 ctx.closePath();
