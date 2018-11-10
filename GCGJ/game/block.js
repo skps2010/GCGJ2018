9527cf74bb461ac732714fe092139e5eb9b68176
BuildTools <unconfigured@null.spigotmc.org>
2018-11-10 21:00:36 +0800

    no message

--- GCGJ/game/block.js
+++ GCGJ/game/block.js
@@ -1,22 +1,68 @@
 var dirList = ["moveUp", "moveDown", "moveLeft", "moveRight"];
 var dirs = ["up", "down", "left", "right"];
 
-for (var i = 0; i < 4; i++) {
-    Blockly.Blocks[dirList[i]] = {
-        init: function() {
-            this.jsonInit({
-                "type": dirList[i],
-                "message0": "移動",
-                "previousStatement": null,
-                "nextStatement": null,
-                "colour": 230,
-                "tooltip": "",
-                "helpUrl": ""
-            });
-        }
-    };
+Blockly.Blocks[dirList[0]] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField(dirs[0]);
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(230);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
+Blockly.Blocks[dirList[1]] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField(dirs[1]);
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(230);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
+Blockly.Blocks[dirList[2]] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField(dirs[2]);
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(230);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
+Blockly.Blocks[dirList[3]] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField(dirs[3]);
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(230);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
 
-    /*Blockly.JavaScript[dirList[i]] = function(block) {
-        return '...;\n';
-    };*/
-}
+Blockly.JavaScript[dirList[0]] = function(block) {
+    move(0);
+    console.log(dirs[0]);
+    return '...;\n';
+};
+Blockly.JavaScript[dirList[1]] = function(block) {
+    move(1);
+    console.log(dirs[1]);
+    return '...;\n';
+};
+Blockly.JavaScript[dirList[2]] = function(block) {
+    move(2);
+    console.log(dirs[2]);
+    return '...;\n';
+};
+Blockly.JavaScript[dirList[3]] = function(block) {
+    move(3);
+    console.log(dirs[3]);
+    return '...;\n';
+};
